a364a3851e875f400787837cfd4d0e78289470c9
jcamposobando <juanignacioco@gmail.com>
2018-08-25 21:09:12 -0600

    a lot of changes

---
+++ _gentags.rb
@@ -0,0 +1,26 @@
+require 'yaml'
+tags = []
+
+dd = Dir.glob(File.join('_posts','*.md'))
+
+puts dd
+print dd
+
+dd.each do |file|
+	yaml_s = File.read(file).split(/^---$/)[1]
+	yaml_h = YAML.load(yaml_s)
+	puts yaml_h['tags']
+	aa = yaml_h['tags'].split(" ")
+	print aa
+	tags += yaml_h['tags'].split(" ")
+end
+
+tags.map(&:downcase).uniq.each do |tag|
+	File.write File.join('tags', "#{tag}.html"), <<-EOF
+---
+layout: tagpage
+title: #{tag}
+permalink: /tags/#{tag}
+---
+	EOF
+end
---
+++ tags/jekyll.html
@@ -0,0 +1,5 @@
+---
+layout: tagpage
+title: jekyll
+permalink: /tags/jekyll
+---
