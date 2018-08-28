776098c892fb0ef6074bfd8b7340913e63ae64cf
jcamposobando <juanignacioco@gmail.com>
2018-08-28 17:33:48 -0600

    moved tag pages out of directory to fix problem with gitHub pages

--- _gentags.rb
+++ _gentags.rb
@@ -16,7 +16,7 @@ dd.each do |file|
 end
 
 tags.map(&:downcase).uniq.each do |tag|
-	File.write File.join('tags', "#{tag}.html"), <<-EOF
+	File.write "#{tag}.html" , <<-EOF
 ---
 layout: tagpage
 title: #{tag}
